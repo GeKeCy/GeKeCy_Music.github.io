65f5bad8212a5d052a91c953f81cfc454bcee85c
GeKeCy <forest2580@qq.com>
2026-07-05 17:22:02 +0000

    重构为Cloudflare Pages结构：移除package.json/node_modules，保留functions后端+静态首页

--- api/search-redirect.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (req, res) => {
-    const keyword = req.query.keyword || '';
-    if (keyword) {
-        res.redirect(301, '/s/' + encodeURIComponent(keyword));
-    } else {
-        res.redirect(301, '/');
-    }
-};
